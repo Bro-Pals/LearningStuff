4269bc9e31283587f76a02c9b73f99ce939bb330
Kevin <kprehn01@gmail.com>
2014-03-18 18:25:51 -0500

    Learning wearning

--- HowDoINetwork/src/howdoinetwork/HowDoIClientSide.java
+++ HowDoINetwork/src/howdoinetwork/HowDoIClientSide.java
@@ -4,10 +4,41 @@
  */
 package howdoinetwork;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Owner
  */
 public class HowDoIClientSide {
     
+    public static void main(String[] args) {
+        
+        String hostName = "Not Found";
+        
+        // find the host name (thanks stack overflow)
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            hostName = address.getHostName();
+        } catch(UnknownHostException e) {
+            System.err.println("Could not get host name "+e.toString());
+        }
+
+        final int portNumber = 55341;
+        
+        try {
+            Socket aSocket = new Socket(hostName, portNumber);
+            PrintWriter writer = new PrintWriter(aSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+        } catch(UnknownHostException e) {
+            System.err.println(e.toString());
+        } catch(Exception e) {
+            System.err.println(e.toString());
+        }
+    }
 }
--- HowDoINetwork/src/howdoinetwork/HowDoINetwork.java
+++ HowDoINetwork/src/howdoinetwork/HowDoINetwork.java
@@ -4,6 +4,12 @@
  */
 package howdoinetwork;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
 /**
  *
  * @author Owner
@@ -14,6 +20,23 @@ public class HowDoINetwork {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        final int portNumber = 55341;
+        
+        try {
+            ServerSocket myFirstServerSocket = new ServerSocket(portNumber);
+            Socket clientSocket = myFirstServerSocket.accept();
+            
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch(IOException e) {
+            System.err.println("IO Exception: "+e.toString());
+        } catch(SecurityException e) {
+            System.err.println("Security Exception!: "+e.toString());
+        } catch(Exception e) {
+            System.err.println(e.toString());
+        }
+        System.out.println("Looks like we have a server now at port "+portNumber);
+        
     }
 }
